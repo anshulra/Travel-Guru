b4c26ad4321ca0260fa4110d068bc00734e1ccbe
anshulra <anshulra@usc.edu>
2015-04-26 04:03:14 -0700

    pos tagg reviews

--- Feature_Ranking/pickle_review.py
+++ Feature_Ranking/pickle_review.py
@@ -3,6 +3,8 @@ import os,sys,nltk,re,pickle
 def crawlData(sourceDir, destDir):
 	sourceFiles = os.listdir(sourceDir)
 	html_tags = re.compile(r'<[^>]+>')
+	amp = re.compile(r'&.+;')
+	count = 0
 	for file in sourceFiles:
 		filepath = sourceDir+"/"+file;
 		inFile = open(filepath,'r')
@@ -21,15 +23,20 @@ def crawlData(sourceDir, destDir):
 				foundreview = line.find('<p itemprop="description" lang="en">',0)
 				if foundreview > -1:
 					review = html_tags.sub(' ',line)
+					review = amp.sub(' ',review)
 					review = nltk.sent_tokenize(review)
 					review = [nltk.word_tokenize(sent) for sent in review]
 					review = [nltk.pos_tag(sent) for sent in review]
+					#review = [' '.join([word[0]+"/"+word[1] for word in sent]) for sent in review]
 					data.append([rating,review])
 					skipLine = False
 		outFile = open(destDir+"/"+file,'wb')
+		#outFile.write(str(data))
 		pickle.dump(data,outFile)
 		outFile.close()
 		inFile.close()
+		count += 1
+		sys.stderr.write(str(count*100/len(sourceFiles)) +"% completed.\n")
 
 if __name__ == "__main__":
 	crawlData(str(sys.argv[1]),str(sys.argv[2]))
